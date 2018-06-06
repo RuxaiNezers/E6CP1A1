c8a01e20cc01215b1262c6a48a78d5761293ac75
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-06-05 20:17:22 -0400

    ciclos exercise 3 solved

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,23 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+a = 990
+b = 1
+
+while (b<=a)
+	if a % b ==0
+		puts b
+	end
+	b+=1
+end
+
+for i in 1..a
+	if a % i ==0
+		puts i
+	end
+end
+
+990.times do |n|
+	if a % (n+1) ==0
+		puts n+1
+	end
+end	
